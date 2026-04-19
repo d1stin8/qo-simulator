93926924396b8f01fab3277c0e023b08a2de11ed
= <=>
2026-04-19 09:02:48 +0530

    vite.config.ts changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
-  base: '/qo-simulator',
+  base: '/qo-simulator/',
   plugins: [solid()],
 })
